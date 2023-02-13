ca244f6ae0a49274d8363487c0ba49c69c080035
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-13 11:13:38 +0100

    Add ProductStoreSeeder.php

---
+++ database/seeders/ProductStoreSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use App\Models\ProductStore;
+use App\Models\Store;
+use Illuminate\Database\Seeder;
+
+class ProductStoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $stores = Store::paginate(2);
+        $products = Product::paginate(2);
+
+        foreach ($products as $product) {
+            foreach ($stores as $store) {
+                ProductStore::firstOrCreate([
+                    'product_id' => $product->id,
+                    'store_id' => $store->id,
+                ]);
+            }
+        }
+    }
+}

e4c4552a90588ee179a49ef06642e5ef803e4557
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-13 11:15:29 +0100

    Add Model: Product & ProductFactory

---
+++ app/Models/Product.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Database\Factories\ProductFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $name
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Store> $stores
+ * @property-read int|null $stores_count
+ * @method static ProductFactory factory($count = null, $state = [])
+ * @method static Builder|Product newModelQuery()
+ * @method static Builder|Product newQuery()
+ * @method static Builder|Product query()
+ * @method static Builder|Product whereCreatedAt($value)
+ * @method static Builder|Product whereId($value)
+ * @method static Builder|Product whereName($value)
+ * @method static Builder|Product whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    const PRODUCTS_BASE_URL = '/products';
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $hidden = [
+        'deleted_at',
+    ];
+
+    public function stores(): BelongsToMany
+    {
+        return $this->belongsToMany(Store::class)
+            ->using(ProductStore::class)
+            ->withPivot(['stock']);
+    }
+}

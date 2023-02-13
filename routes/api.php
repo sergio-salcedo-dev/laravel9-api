05e489eed5feb29a6c89eab9c9e352cef750a9ed
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-13 12:01:47 +0100

    Add Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
+use App\Models\Product;
+use App\Models\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/*
+|--------------------------------------------------------------------------
+| User Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth:sanctum')->get('/user', fn(Request $request) => $request->user());
+
+/*
+|--------------------------------------------------------------------------
+| Stores Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(
+    [
+//        'middleware' => 'auth:sanctum'
+    ],
+    function () {
+        Route::group(
+            ['prefix' => Store::STORES_BASE_URL],
+            function () {
+                Route::get('/products', [StoreController::class, 'storesWithProducts'])->name('stores.products');
+                Route::get('/{id}/products', [StoreController::class, 'storeWithProducts'])->name('store.products');
+                Route::get('/products-count', [StoreController::class, 'storesWithProductsCount'])
+                    ->name('stores.products-count');
+            }
+        );
+        Route::apiResource(Store::STORES_BASE_URL, StoreController::class);
+    }
+);
+
+/*
+|--------------------------------------------------------------------------
+| Products Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(
+    [
+//        'middleware' => 'auth:sanctum'
+    ],
+    function () {
+        Route::group(
+            ['prefix' => Product::PRODUCTS_BASE_URL],
+            function () {
+                Route::post('/sell', [ProductController::class, 'sell'])->name('products.sell');
+            }
+        );
+        Route::apiResource(Product::PRODUCTS_BASE_URL, ProductController::class);
+    }
+);

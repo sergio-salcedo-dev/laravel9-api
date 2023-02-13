1b7c54aeed05d25179cf26db6e8f2464a060fd77
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-13 11:54:32 +0100

    Add StoreSellProductRequest

---
+++ app/Http/Requests/StoreSellProductRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+class StoreSellProductRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+//        return !!Auth::user();
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            "storeId" => "required|numeric|integer|gt:0",
+            "productId" => "required|numeric|integer|gt:0",
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     */
+    public function messages(): array
+    {
+        return [
+            'store_id.required' => 'the store_id is required',
+            'product_id.required' => 'the product_id is required',
+        ];
+    }
+}

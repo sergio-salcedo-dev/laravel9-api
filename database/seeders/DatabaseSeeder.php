8d51d876957b61b5fc4af247119b462efb765614
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-13 11:17:14 +0100

    Add seeders to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,24 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Hash;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    /** Seed the application's database  */
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(1)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $this->call(
+            [
+                ProductSeeder::class,
+                StoreSeeder::class,
+                ProductStoreSeeder::class,
+            ]
+        );
     }
 }

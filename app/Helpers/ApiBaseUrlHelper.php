248bdb06f4d1814b0d038a12b574271859dec496
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-18 21:30:50 +0100

    Add ApiBaseUrlHelper.php

---
+++ app/Helpers/ApiBaseUrlHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+class ApiBaseUrlHelper
+{
+    public const STORES_BASE_URL = "/stores";
+    public const PRODUCTS_BASE_URL = '/products';
+}

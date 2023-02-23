10c2148fad82249fad9c96ae908f1036ea2036f4
Sergio Salcedo <sergio.salcedo.dev@gmail.com>
2023-02-23 12:39:03 +0100

    Add middleware 'guest' to login & register controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
+    protected $middleware = ['guest'];
+
     public const TOKEN_NAME = 'api_token';
 
     public function __invoke(UserLoginRequest $request): User|Response
